c244af663c839507931c0cc3562fe9741d11269e
Mad31 <jocelin.devalette@egd.mg>
2014-06-20 14:49:52 +0300

    commentaire
    Juste quelques commentaires en plus

--- commbot.py
+++ commbot.py
@@ -39,7 +39,7 @@ def printVersion(brick):
 def Commande(i):
     # intialisation du capteur de lumière
     lumiere.set_illuminated(True)
-    sleep(0.1)
+    sleep(0.1) # il faut attendre que le capteur s'allume avant de faire la mesure
     lumens=lumiere.get_lightness()
     print lumens
     global mur
@@ -75,7 +75,7 @@ def Commande(i):
     """ le 1 reculer"""
     if i==2 :
         deux.turn(-75,500,True)
-    lumiere.set_illuminated(False)
+    lumiere.set_illuminated(False) # on eteint le capteur, pas la peine de consommer trop !
     print mur
     
 def InitComm():
@@ -83,13 +83,13 @@ def InitComm():
     brick=rechercher_brique("NXT5")
     gauche = nxt.Motor(brick, PORT_B)
     droite = nxt.Motor(brick, PORT_C)
-    global deux
-    global deuxgauche
-    global deuxdroite
+    global deux # actionneurs servo droite/gauche pour aller tout droit
+    global deuxgauche # actionneur servos gauche/droite pour tourner à gauche
+    global deuxdroite # actionneur servos droite/gauche pour tourner à droite
     deux = nxt.SynchronizedMotors(gauche,droite,0)
     deuxgauche = nxt.SynchronizedMotors(gauche, droite, 100)
     deuxdroite = nxt.SynchronizedMotors(droite, gauche, 100)
-    global lumiere
+    global lumiere # objet capteur de lumière
     lumiere = nxt.Light(brick, PORT_1)
     
 def CodeBrick():
